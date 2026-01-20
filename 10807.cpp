91616da2454e03ac6106ead14effb03f56aa6609
Jihunpyo <pyojihun80@gmail.com>
2026-01-20 23:29:28 +0900

    2026.01.20. 공부 시작 전 커밋

--- 10807.cpp
+++ 10807.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
-	// 코드 작성
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin>>n;
+
+	int* a = new int[n];
+
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}	
+	int num,cnt=0;
+	cin>>num;
+	for(int i=0;i<n;i++){
+		if(num==a[i]) cnt++;
+	}
+	cout<<cnt;
+	delete[] a;
 	return 0;
 }
